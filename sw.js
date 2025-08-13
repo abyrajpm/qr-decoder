8e0fda7fd7ce1e376039f8b517d997e36a9f6dc0
abyrajpm <abyrajpm@gmail.com>
2025-08-13 11:53:52 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const ASSETS = ['.', './index.html', './manifest.webmanifest'];
+const ASSETS = ['.', './index.html', './manifest.webmanifest', './config.json'];
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('qr-decoder-v1').then(c=>c.addAll(ASSETS)));
+  e.waitUntil(caches.open('qr-decoder-v3').then(c=>c.addAll(ASSETS)));
 });
 self.addEventListener('fetch', e=>{
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
